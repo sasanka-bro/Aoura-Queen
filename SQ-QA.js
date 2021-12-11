661b2e7ba945748169fd2c6166768ab8f372e0e7
sasanka-bro <93658348+sasanka-bro@users.noreply.github.com>
2021-12-11 16:59:08 +0530

    Delete SQ-QA.js

--- SQ-QA.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-   AOURA_QUEEN_OWNER: '94776135807,94759554531', // Yuren Sasanka - Aoura Queen Owner
-   QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship
-    } 
