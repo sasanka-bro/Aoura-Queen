e064b7f782c6cc595cf4e15ce4bfa843c4714d6d
sasanka-bro <93658348+sasanka-bro@users.noreply.github.com>
2021-12-11 17:01:35 +0530

    Add files via upload

---
+++ SQ-QA.js
@@ -0,0 +1,4 @@
+module.exports = {
+   AOURA_QUEEN_OWNER: '94776135807,94759554531', // Yuren Sasanka - Aoura Queen Owner
+   QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship
+    } 
